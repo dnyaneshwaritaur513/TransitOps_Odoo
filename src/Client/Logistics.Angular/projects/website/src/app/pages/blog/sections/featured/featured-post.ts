c916816a9fc029abe98ebf7b4ca31fd6cbdc4bcd
Nirjala-pagare <nirjalapagare123@gmail.com>
2026-07-12 16:54:36 +0530

    Add documentation for FeaturedPost component
    Added documentation comments for the FeaturedPost component and its methods.

--- src/Client/Logistics.Angular/projects/website/src/app/pages/blog/sections/featured/featured-post.ts
+++ src/Client/Logistics.Angular/projects/website/src/app/pages/blog/sections/featured/featured-post.ts
@@ -7,6 +7,15 @@ import { ScrollAnimateDirective } from "@/shared/directives";
 import { getReadTime } from "@/shared/utils";
 import { BlogStore } from "../../store/blog.store";
 
+/**
+ * FeaturedPost component
+ *
+ * Displays the highlighted blog post section with:
+ * - Post details
+ * - Author information
+ * - Reading time calculation
+ * - Loading skeleton state
+ */
 @Component({
   selector: "web-featured-post",
   templateUrl: "./featured-post.html",
@@ -22,6 +31,13 @@ import { BlogStore } from "../../store/blog.store";
   ],
 })
 export class FeaturedPost {
+  /**
+   * Store responsible for managing blog post data.
+   */
   protected readonly store = inject(BlogStore);
+
+  /**
+   * Utility function to calculate estimated reading time.
+   */
   protected readonly getReadTime = getReadTime;
 }
